2320a96940256e8a7933ec2b495d325bba3fcc51
akshu <in.akr.2000@gmail.com>
2023-08-22 23:32:02 +0530

    Added some styles to Login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import React from "react";
 
 const App = () => {
     return (
         <div className="App">
-            <h2>Lets build Linkedin CLone</h2>
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <Login />
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 };
