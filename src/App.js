d8b35840b2cde6b930a6c560655c00ed23d15ac5
akshu <in.akr.2000@gmail.com>
2023-08-23 10:19:46 +0530

    added home icon and search bar to home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Login from "./components/Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 import React from "react";
 
@@ -11,6 +13,10 @@ const App = () => {
                 <Switch>
                     <Route exact path="/">
                         <Login />
+                    </Route>
+                    <Route path="/home">
+                        <Header />
+                        <Home/>
                     </Route>
                 </Switch>
             </Router>
